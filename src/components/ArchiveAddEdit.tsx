80d2f392ad53fdc85bfa52b740593f63c22ff14f
minn0001 <kelasapdb2024@gmail.com>
2026-06-11 22:12:38 +0700

    Update ArchiveAddEdit.tsx

--- src/components/ArchiveAddEdit.tsx
+++ src/components/ArchiveAddEdit.tsx
@@ -5,11 +5,6 @@
 
 import React, { useState, useEffect } from "react";
 import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  'https://mdcoxcwtgveczzvbjwzx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kY294Y3d0Z3ZlY3p6dmJqd3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNDE4MTQsImV4cCI6MjA5NjcxNzgxNH0.CsfMpWulQPrizMRyzZVleSC17nIHtZVO2Ru_8H7kQgs'
-);
 import { 
   UploadCloud, 
   Trash, 
@@ -24,6 +19,11 @@ import {
   History
 } from "lucide-react";
 import { Archive, KategoriArsip, StatusArsip, User, DocumentVersion } from "../types";
+
+const supabase = createClient(
+  'https://mdcoxcwtgveczzvbjwzx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kY294Y3d0Z3ZlY3p6dmJqd3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNDE4MTQsImV4cCI6MjA5NjcxNzgxNH0.CsfMpWulQPrizMRyzZVleSC17nIHtZVO2Ru_8H7kQgs'
+);
 
 interface ArchiveAddEditProps {
   mode: "add" | "edit";
