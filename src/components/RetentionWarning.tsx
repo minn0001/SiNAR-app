b87cf584a4f6c18bfea49db093838036c026b284
minn0001 <kelasapdb2024@gmail.com>
2026-06-28 22:04:39 +0700

    Update RetentionWarning.tsx

--- src/components/RetentionWarning.tsx
+++ src/components/RetentionWarning.tsx
@@ -19,13 +19,7 @@ import {
   X
 } from "lucide-react";
 import { Archive, StatusArsip, User, UserRole } from "../types";
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  'https://mdcoxcwtgveczzvbjwzx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kY294Y3d0Z3ZlY3p6dmJqd3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNDE4MTQsImV4cCI6MjA5NjcxNzgxNH0.CsfMpWulQPrizMRyzZVleSC17nIHtZVO2Ru_8H7kQgs'
-);
+import { supabase } from '../lib/supabaseClient';
 
 interface RetentionWarningProps {
   archives: Archive[];
